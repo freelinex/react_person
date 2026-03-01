d0fc364dcee409d9caa84332023f7a1852e9dcb8
freelinex <freelinex7@gmail.com>
2026-03-01 17:30:03 +0200

    correcting task solution2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,5 +1,8 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
+  const ageCheck = age !== undefined && (
+    <p className="Person__age">{`I am ${age}`}</p>
+  );
   const partnerSex = sex === 'm' ? 'wife' : 'husband';
   const status = isMarried
     ? `${partnerName} is my ${partnerSex}`
@@ -8,7 +11,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
+      {ageCheck}
       <p className="Person__partner">{status}</p>
     </section>
   );
